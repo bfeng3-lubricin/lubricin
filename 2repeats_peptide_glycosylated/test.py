dab41ab894371c4f93a0e3ff75c284616e5f63e3
bibo_feng@brown.edu <bfeng3@node1902.oscar.ccv.brown.edu>
2025-06-01 01:39:15 -0400

    added a bunch of stuff

--- 2repeats_peptide_glycosylated/test.py
+++
@@ -1,5 +0,0 @@
-factor = 302.0/300.0
-temp = 300
-for i in range(64):
-   print(temp)
-   temp = temp*factor
--- scripts/threonines_populations.py
+++ scripts/threonines_populations.py
@@ -6,7 +6,6 @@ from scipy.stats import sem
 plt.rcParams["font.family"] = "Times New Roman"
 
 
-
 lys2=[]
 glu3=[]
 pro4=[]
@@ -26,7 +25,6 @@ pro17=[]
 
 peptide=[lys2,glu3,pro4,ala5,pro6,thr7,thr8,thr9,lys10,glu11,pro12,ala13,pro14,thr15,thr16,pro17]
 
-
 lys2g=[]
 glu3g=[]
 pro4g=[]
@@ -44,18 +42,18 @@ thr15g=[]
 thr16g=[]
 pro17g=[] 
 
-equilibration_time = 0 #100000
 glycopeptide=[lys2g,glu3g,pro4g,ala5g,pro6g,thr7g,thr8g,thr9g,lys10g,glu11g,pro12g,ala13g,pro14g,thr15g,thr16g,pro17g]
+
 for res_num in range(1,17):
    res_dihedral_file = open("noglycan/rama_res"+str(res_num)+".dat")
    read_file = res_dihedral_file.readlines() 
-   for i in range(equilibration_time,len(read_file)):
+   for i in range(0,len(read_file)):
        peptide[res_num-1].extend([read_file[i]])
 
 for res_num in range(1,17):
    res_dihedral_file = open("withglycan/rama_res"+str(res_num)+".dat")
    read_file = res_dihedral_file.readlines()
-   for i in range(equilibration_time,len(read_file)):
+   for i in range(0,len(read_file)):
        glycopeptide[res_num-1].extend([read_file[i]])
 
 def structure_population(dihedrals,phi1,phi2,psi1,psi2): #list of dihedrals (in string format separated by space); ranges of phi and psi angles for a specified type of structure
@@ -94,8 +92,6 @@ for i in range(len(peptide)):
      beta_stde.append([structure_population(peptide[i],-180,-90,50,240)[1],structure_population(glycopeptide[i],-180,-90,50,240)[1]])
      r_alpha_population.append([structure_population(peptide[i],-160,-20,-120,50)[0],structure_population(glycopeptide[i],-160,-20,-120,50)[0]])
      r_alpha_stde.append([structure_population(peptide[i],-160,-20,-120,50)[1],structure_population(glycopeptide[i],-160,-20,-120,50)[1]])
-#   l_alpha_population.append([structure_population(peptide[i],30,100,0,80)[0],structure_population(glycopeptide[i],30,100,0,80)[0]])
-#   l_alpha_stde.append([structure_population(peptide[i],30,100,0,80)[1],structure_population(glycopeptide[i],30,100,0,80)[1]])
 
 
 PPII_population = np.array(PPII_population)
@@ -107,8 +103,6 @@ r_alpha_stde = np.array(r_alpha_stde)
 l_alpha_population = np.array(l_alpha_population)
 l_alpha_stde = np.array(l_alpha_stde)
 
-print(beta_population)
-print(beta_stde)
 
 fig, axs = plt.subplots(nrows=3,ncols=1,sharex=True) 
 axs = axs.flatten()
@@ -124,19 +118,16 @@ axs[1].bar(x-width/2,beta_population[:,0],width,label="Peptide",yerr=beta_stde[:
 axs[1].bar(x+width/2,beta_population[:,1],width,label="Glycopeptide",yerr=beta_stde[:,1],color="red",capsize=3)
 axs[2].bar(x-width/2,r_alpha_population[:,0],width,yerr=r_alpha_stde[:,0],color="purple",capsize=3)
 axs[2].bar(x+width/2,r_alpha_population[:,1],width,yerr=r_alpha_stde[:,1],color="red",capsize=3)
-#axs[3].bar(x-width/2,l_alpha_population[:,0],width,yerr=l_alpha_stde[:,0],color="purple",capsize=3)
-#axs[3].bar(x+width/2,l_alpha_population[:,1],width,yerr=l_alpha_stde[:,1],color="red",capsize=3)
 
 plt.xticks(x,labels,rotation='vertical')
 axs[0].set_ylabel("PPII")#,fontweight="bold")
 axs[1].set_ylabel(r'$\beta$' ,fontweight="bold")
 axs[2].set_ylabel(r'$\alpha_R$' ,fontweight="bold")
-#axs[3].set_ylabel('Left-hand Alpha Helix' ,fontweight="bold")
 
 axs[0].set_ylim(0,1)
 axs[1].set_ylim(0,1)
 axs[2].set_ylim(0,1)
-#axs[3].set_ylim(0,1)
+
 plt.xlabel("Residue",fontweight="bold")
 axs[1].legend()
 plt.savefig("PPII_population_change.png",dpi=600)
